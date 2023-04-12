5a9467d59a6401887b7f8ddf1f3a06cff928f6bf
dat.ngo <dat.ngo@fusionstar.tech>
2023-04-12 11:59:32 +0800

    simple react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,59 @@
-function App() {
+import {
+  createBrowserRouter,
+  Link,
+  Outlet,
+  RouterProvider,
+  useParams,
+} from 'react-router-dom';
+import Login from './pages/login/LoginPage';
+
+function Home() {
   return (
-    <div>
-      <p className='text-center'>Hello world</p>
-    </div>
+    <>
+      <div id='sidebar'>
+        <h1>React Router Contacts</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={`contacts/1`}>Your Name</Link>
+            </li>
+            <li>
+              <Link to={`contacts/2`}>Your Friend</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div id='detail'>
+        <Outlet />
+      </div>
+    </>
   );
 }
 
-export default App;
+function Contact() {
+  const { contactId } = useParams();
+  return <>ContactID: {contactId}</>;
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: 'Page not found',
+  },
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: 'Page not found',
+    children: [
+      {
+        path: 'contacts/:contactId',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
---
+++ src/pages/login/LoginPage.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div>
+      <h3>Login</h3>
+      <p>This is login page</p>
+    </div>
+  );
+}
